02384c59eb2b9bd8c9a6175b303c4193e064479a
m4rche <marcelmuslija@pm.me>
2026-05-14 20:27:53 +0200

    clangd config changes

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -1,8 +1,8 @@
 return {
     cmd = { 
         'clangd',
-        '--fallback-style=GNU',
-        '--header-insertion=iwyu'
+        '--fallback-style=LLVM',
+        '--header-insertion=never',
     },
     filetypes = { 'c', 'cpp'},
     root_markers = { 'compile_commands.json', 'compile_flags.txt', 'CMakeLists.txt', '.git'}
