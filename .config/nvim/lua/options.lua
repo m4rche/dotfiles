94021f67c58e5bff8ff02f1cf588976f83aa8bb9
m4rche <marcelmuslija@pm.me>
2026-05-14 20:28:11 +0200

    tab width

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -10,9 +10,9 @@ vim.g.maplocalleader = " "
 vim.opt.clipboard = 'unnamedplus'
 
 -- Tab
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 -- UI
