7d3406d01c9dd8d256151ef44801d0e10419a368
m4rche <marcelmuslija@pm.me>
2026-05-24 11:09:13 -0400

    add prompt; change active border color

---
+++ .zsh/prompt.zsh
@@ -0,0 +1,22 @@
+RESET='%f'
+WHITE='%F{white}'
+GREEN='%F{green}'
+BLUE='%F{orange}'
+
+if [[ $EUID -eq 0 ]]; then
+  USER_COLOR=$GREEN
+else
+  USER_COLOR=$WHITE
+fi
+
+virtualenv_prompt() {
+  [[ -n "$VIRTUAL_ENV" ]] && echo "${GREEN}[$(basename $VIRTUAL_ENV)]${RESET}"
+}
+
+git_branch_prompt() {
+  branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  [[ -n "$branch" ]] && echo "%F{208}[$branch]%f"
+}
+
+PROMPT="${WHITE}┌──(${USER_COLOR}%n@%m${WHITE})-[%~]$(virtualenv_prompt)$(git_branch_prompt)
+└─${GREEN}# ${RESET}"
